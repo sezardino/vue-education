2f63b7e893724a46f192993b5e8d47da8c31d8eb
Edward <e.arechwa@gmail.com>
2021-05-06 20:56:13 +0200

    add handlers

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log(Vue);
+const App = {
+  data() {
+    return {
+      title: "ToDo List",
+      placeholderText: "Add todo",
+      inputValue: "",
+      notes: [],
+    };
+  },
+  methods: {
+    inputHandler(evt) {
+      this.inputValue = evt.target.value;
+    },
+    submitHandler(evt) {
+      evt.preventDefault();
+      this.notes.push(evt.target.value);
+    },
+  },
+};
+
+const app = Vue.createApp(App);
+app.mount("#app");
