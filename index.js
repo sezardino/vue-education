a4e7cd57d29564a2bd24979cb6ad66861ad548a5
Edward <edward.arechwa@gogomedia.pl>
2021-05-07 09:09:34 +0200

    v-model

--- index.js
+++ index.js
@@ -1,38 +1,35 @@
 const App = {
-  data() {
-    return {
-      title: "ToDo List",
-      placeholderText: "Add todo",
-      inputValue: "",
-      notes: [],
-    };
-  },
-  methods: {
-    inputHandler(evt) {
-      this.inputValue = evt.target.value;
+    data() {
+        return {
+            title: "ToDo List",
+            placeholderText: "Add todo",
+            inputValue: "",
+            notes: [],
+        };
     },
-    submitHandler(evt) {
-      evt.preventDefault();
-      if (this.inputValue !== "") {
-        this.notes.push(this.inputValue);
-        this.inputValue = "";
-      }
+    methods: {
+        submitHandler(evt) {
+            evt.preventDefault();
+            if (this.inputValue !== "") {
+                this.notes.push(this.inputValue);
+                this.inputValue = "";
+            }
+        },
+        removeHandler(index) {
+            this.notes.splice(index, 1);
+        },
     },
-    removeHandler(index) {
-      this.notes.splice(index, 1);
+    computed: {
+        dabbledLength() {
+            console.log("enter");
+            return this.notes.length * 2;
+        },
     },
-  },
-  computed: {
-    dabbledLength() {
-      console.log("enter");
-      return this.notes.length * 2;
+    watch: {
+        inputValue(value) {
+            console.log("New value: ", value);
+        },
     },
-  },
-  watch: {
-    inputValue(value) {
-      console.log("New value: ", value);
-    },
-  },
 };
 
 const app = Vue.createApp(App);
