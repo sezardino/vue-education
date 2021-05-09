fee4c8b83bb3428a5c8a6f45d680f7b4ab054e02
Edward <e.arechwa@gmail.com>
2021-05-09 10:06:13 +0200

    remaster: translate on english

--- src/App.vue
+++ src/App.vue
@@ -30,7 +30,7 @@
         <div class="flex">
           <div class="max-w-xs">
             <label for="wallet" class="block text-sm font-medium text-gray-700"
-              >Тикер</label
+              >Tiker</label
             >
             <div class="mt-1 relative rounded-md shadow-md">
               <input
@@ -58,7 +58,7 @@
               </template>
             </div>
             <div v-show="validate" class="text-sm text-red-600">
-              Такой тикер уже добавлен
+              This ticker has already been added
             </div>
           </div>
         </div>
@@ -80,7 +80,7 @@
               d="M13 7h-2v4H7v2h4v4h2v-4h4v-2h-4V7zm-1-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"
             ></path>
           </svg>
-          Добавить
+          Add
         </button>
       </section>
       <template v-if="tikers.length">
@@ -118,7 +118,7 @@
                   d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
                   clip-rule="evenodd"
                 ></path></svg
-              >Удалить
+              >Delete
             </button>
           </div>
         </dl>
